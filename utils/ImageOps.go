2fa1e74177a82d3813575dafe63ea908d9327e36
Roberzzt <robertzel_2001@yahoo.com>
2023-07-27 01:10:12 +0300

    encoding png images

--- utils/ImageOps.go
+++ utils/ImageOps.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nfnt/resize"
 	"image"
 	"image/jpeg"
+	_ "image/png"
 	"os"
 )
 
@@ -17,7 +18,7 @@ func Resize(filepath string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	newImage := resize.Resize(256, 256, img, resize.Lanczos3)
+	newImage := resize.Resize(128, 128, img, resize.Lanczos3)
 	buffer := new(bytes.Buffer)
 
 	if err = jpeg.Encode(buffer, newImage, nil); err != nil {
